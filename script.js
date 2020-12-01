0064047dfbbc346b7e63f1a394f7f832afa28dd3
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2020-12-01 10:35:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ //Functions Here
+ function o(i){
+    return document.getElementById(i);
+ }
+
+ function img(url , size){
+     return "<img src='"+ url +"' height='"+ size +"'>";
+ }
+ 
+ //Other Feature Functions
+ function DisableRClick(){
+ document.addEventListener('contextmenu', event => event.preventDefault());
+ }
+ o('sd-fb').oncontextmenu = function return_false() { return false };
+
+
+ //Images Here
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+ o('sd-fb').innerHTML = img('https://avatars1.githubusercontent.com/u/9919?s=200&v=4' , o('sd-fb').style.fontSize) ;
+
+ 
+
+ 
+
+
+// Github = https://avatars1.githubusercontent.com/u/9919?s=200&v=4
